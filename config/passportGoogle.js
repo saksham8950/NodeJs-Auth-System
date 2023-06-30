a9b764879970d53e86f55241c5f0a36a8513fdef
Saksham <107344612+saksham8950@users.noreply.github.com>
2023-07-01 01:45:28 +0700

    Update passportGoogle.js

--- config/passportGoogle.js
+++ config/passportGoogle.js
@@ -7,8 +7,8 @@ const crypto = require('crypto');
 module.exports.passport = passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:  process.env.GOOGLE_CALLBACK_URL_LOCAL,
-    // callbackURL:  process.env.GOOGLE_CALLBACK_URL,
+    // callbackURL:  process.env.GOOGLE_CALLBACK_URL_LOCAL,
+    callbackURL:  process.env.GOOGLE_CALLBACK_URL,
     passReqToCallback   : true
   }, (request, accessToken, refreshToken, profile, done) => {
 
