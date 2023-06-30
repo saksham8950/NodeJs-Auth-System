874cddf1aefea31be6c99c6c43a06d1cca814438
Saksham <sakshamchopra174@gmail.com>
2023-06-30 23:20:51 +0530

    Cloud Databse Connected

--- config/db.js
+++ config/db.js
@@ -1,9 +1,31 @@
+// const mongoose = require('mongoose');
+
+// // connection to mongoDB
+// mongoose.connect('mongodb://127.0.0.1/Login-Auth')
+
+// //Error Handling
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console,'error connecting to db'));
+// db.once('open', () => { console.log('Successfully connected to the database')});
+
+
 const mongoose = require('mongoose');
+require("dotenv").config();
 
-// connection to mongoDB
-mongoose.connect('mongodb://127.0.0.1/Login-Auth')
+// cloud connection-Str
+let cloudDB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
 
-//Error Handling
-const db = mongoose.connection;
-db.on('error', console.error.bind(console,'error connecting to db'));
-db.once('open', () => { console.log('Successfully connected to the database')});
+// mongoose.connect(process.env.DB_LOCAL, {
+mongoose.connect(cloudDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log('DB connect success :)');
+}).catch(() => {
+  console.log('Error to connect DB !!!');
+});
--- config/passportGoogle.js
+++ config/passportGoogle.js
@@ -7,7 +7,8 @@ const crypto = require('crypto');
 module.exports.passport = passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5001/users/auth/google/callback",
+    callbackURL:  process.env.GOOGLE_CALLBACK_URL_LOCAL,
+    // callbackURL:  process.env.GOOGLE_CALLBACK_URL,
     passReqToCallback   : true
   }, (request, accessToken, refreshToken, profile, done) => {
 
